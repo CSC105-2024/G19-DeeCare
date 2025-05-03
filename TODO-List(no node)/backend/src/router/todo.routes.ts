fa3af267a2f5ba9c4b7ece345f9d811d21875673
BBelleysp <yossapan.r@gmail.com>
2025-05-03 23:17:09 +0700

    Del in my path

--- TODO-List(no node)/backend/src/router/todo.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-
-const todoRouter = new Hono();
-
-todoRouter.get("/", (c) => c.text("Hello From Todo Router"));
-todoRouter.get("/:id", (c) => c.text("Hello From Todo Router"));
-todoRouter.post("/", (c) => c.text("Hello From Todo Router"));
-todoRouter.delete("/:id", (c) => c.text("Hello From Todo Router"));
-export { todoRouter };
