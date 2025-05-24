2bf69615b59b0986b8767b19bc96c2e113c41000
MOBEE 모비 <152620609+SNOWxSAUSAGES@users.noreply.github.com>
2025-05-24 22:34:10 +0700

    Update doctor.controller.ts

--- backend/src/controllers/doctor.controller.ts
+++ backend/src/controllers/doctor.controller.ts
@@ -3,13 +3,40 @@ import {doctorModel} from "../models/doctor.model.js";
 
 export const doctorController = {
     // Get all doctors
+    deleteDoc : async (c: Context) => {
+        try {
+        const id = await c.req.param('id')
+        if (!id) {
+            return c.json({ error: "id is required" }, 404);
+          }
+          const deleteDoc = await doctorModel.DeleteDoctor(parseInt(id));
+            return c.json({
+                success: true,
+                msg: "Delete success!",
+            });
+        } catch (e) {
+        return c.json(
+            {
+            success: false,
+            data: null,
+            msg: `Internal Server Error : ${e}`,
+            },
+            500
+        );
+        }
+    },
+
     getAllDoctors: async (c: Context) => {
         try {
-            const doctors = await doctorModel.findAll();
-            return c.json({doctors}, 200);
+            const doctors = await doctorModel.findAllDoc();
+            return c.json({
+                success: true,
+                data: doctors,
+                msg: "Doctor in this department!",
+            });
         } catch (error) {
-            console.error("Error fetching doctors:", error);
-            return c.json({error: "Failed to fetch doctors"}, 500);
+            console.error("Error fetching doctor:", error);
+            return c.json({error: "Failed to fetch doctor"}, 500);
         }
     },
 
@@ -32,6 +59,53 @@ export const doctorController = {
         } catch (error) {
             console.error("Error fetching doctor:", error);
             return c.json({error: "Failed to fetch doctor"}, 500);
+        }
+    },
+    getDocbyDepartment: async (c: Context) => {
+        try {
+        const department = await c.req.param('department')
+        if (!department) {
+            return c.json({ error: "department is required" }, 404);
+          }
+          const getDocDE = await doctorModel.findByde(department);
+            return c.json({
+                success: true,
+                data: getDocDE,
+                msg: "Doctor in this department!",
+            });
+        } catch (e) {
+        return c.json(
+            {
+            success: false,
+            data: null,
+            msg: `Internal Server Error : ${e}`,
+            },
+            500
+        );
+        }
+    },
+
+    getDocbySearch: async (c: Context) => {
+        try {
+        const name = await c.req.param('name')
+        if (!name) {
+            return c.json({ error: "department is required" }, 404);
+          }
+          const getDocname = await doctorModel.findBySearch(name);
+            return c.json({
+                success: true,
+                data: getDocname,
+                msg: "Doctor in this department!",
+            });
+        } catch (e) {
+        return c.json(
+            {
+            success: false,
+            data: null,
+            msg: `Internal Server Error : ${e}`,
+            },
+            500
+        );
         }
     },
 
